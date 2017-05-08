afd8a90ff7ee7e5a349e3870c4243f04c2afc5e2
Kang <2881570+kangni@users.noreply.github.com>
2017-05-08 14:47:36 +0800

    visitor friendly

--- chat.py
+++ chat.py
@@ -3,6 +3,10 @@
 import tornado.ioloop
 import tornado.options
 import tornado.web
+import tornado.websocket
+import tornado.escape
+import logging
+import uuid
 import os.path
 from tornado.options import define, options
 
@@ -14,19 +18,62 @@ class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r"/", MainHandler),
+            (r"/chatsocket", ChatSocketHandler),
         ]
         settings = dict(
-            cookie_secret = "FORBIDDEN",
-            static_path = os.path.join(os.path.dirname(__file__), "static"),
-            template_path = os.path.join(os.path.dirname(__file__), "templates"),
-            xsrf_cookies = True,
+            cookie_secret="FORBIDDEN",
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            xsrf_cookies=True,
         )
         super(Application, self).__init__(handlers, **settings)
 
 
 class MainHandler(tornado.web.RequestHandler):
+    def data_received(self, chunk):
+        pass
+
     def get(self):
-        self.render("index.html")
+        self.render("index.html", username="visitor %d" % ChatSocketHandler.client_id)
+
+
+class ChatSocketHandler(tornado.websocket.WebSocketHandler):
+    def data_received(self, chunk):
+        pass
+
+    waiters = set()
+    client_id = 1
+
+    def open(self):
+        self.client_id = ChatSocketHandler.client_id
+        ChatSocketHandler.client_id += 1
+        ChatSocketHandler.waiters.add(self)
+
+    def on_close(self):
+        ChatSocketHandler.waiters.remove(self)
+
+    def on_message(self, message):
+        logging.info("got message %r", message)
+        parsed = tornado.escape.json_decode(message)
+        self.username = parsed["username"]
+        chat = {
+            "id": str(uuid.uuid4()),
+            "body": parsed["body"],
+            "type": "message",
+        }
+        chat["html"] = tornado.escape.to_basestring(
+            self.render_string("message.html", message=chat)
+        )
+        ChatSocketHandler.send_updates(chat)
+
+    @classmethod
+    def send_updates(cls, chat):
+        logging.info("sending message to %d waiters", len(cls.waiters))
+        for waiter in cls.waiters:
+            try:
+                waiter.write_message(chat)
+            except:
+                logging.error("Error sending message", exc_info=True)
 
 
 def main():
